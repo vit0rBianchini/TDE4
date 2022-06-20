64d8cd035971a90556939e55b0f8cd46cb820a91
Vitor Bianchini de Quadros <vitorbiqua@outlook.com>
2022-06-20 18:05:44 -0300

    👍

--- src/App.java
+++ src/App.java
@@ -8,41 +8,66 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         long start;
-        int n = 10000;
+        int n = 1000000;
         
         int[] vetor = new int[n];
         
         int[] vetorAleatorio = insereAleatorio(vetor, n);
         int[] vetorDescente = insereDecrescente(vetor, n);
 
+        System.out.println("+--------------------------------------------------------------+");
+        System.out.println("|  Tamanho do conjunto:"+n);
+        System.out.println("|               |"+"       Tempos obtidos pelos algoritimos       "+"|");
+        System.out.println("|   Algoritmo   | Quase ordenado |  Desordenado |  Ord. Desc   |");
+
         // // SHELLSORT
-        // start = System.currentTimeMillis();
-        // ShellSort.shellsort(vetorAleatorio);
-        // tempoAleatorio = (float) (System.currentTimeMillis() - start) / 1000;
-
-        // System.out.println("Tempo de ordenação aleatório: " + tempoAleatorio + " segundos");
-
-
-        // start = System.currentTimeMillis();
-        // ShellSort.shellsort(vetorDescente);
-        // tempoOrdenado = (float) (System.currentTimeMillis() - start) / 1000;
-
-        // System.out.println("Tempo de ordenação ordenado: " + tempoOrdenado + " segundos");
-
-        // QUICKSORT
-
         start = System.currentTimeMillis();
-        Quicksort.quickSort(vetorAleatorio, 0, n - 1);
+        ShellSort.shellsort(vetorAleatorio);
         tempoAleatorio = (float) (System.currentTimeMillis() - start) / 1000;
 
-        System.out.println("Tempo de ordenação aleatório: " + tempoAleatorio + " segundos");
-
         start = System.currentTimeMillis();
-        Quicksort.quickSort(vetorDescente, 0, n - 1);
+        ShellSort.shellsort(vetorDescente);
         tempoOrdenado = (float) (System.currentTimeMillis() - start) / 1000;
 
-        System.out.println("Tempo de ordenação ordenado: " + tempoOrdenado + " segundos");
+        String mensagem = String.format("|   SHELLSORT   | Quase ordenado |     %.3f    |     %.3f    |", tempoAleatorio, tempoOrdenado);
 
+        System.out.println(mensagem);
+
+        // QUICKSORT
+        // start = System.currentTimeMillis();
+        // int size = vetorAleatorio.length;
+        // Quicksort.quickSort(vetorAleatorio, 0, size - 1);
+        // tempoAleatorio = (float) (System.currentTimeMillis() - start) / 1000;
+
+        // HEAPSORT
+        HeapSort h = new HeapSort();
+
+        start = System.currentTimeMillis();
+        h.sort(vetorAleatorio);
+        tempoAleatorio = (float) (System.currentTimeMillis() - start) / 1000;
+
+        start = System.currentTimeMillis();
+        h.sort(vetorDescente);
+        tempoOrdenado = (float) (System.currentTimeMillis() - start) / 1000;
+
+        mensagem = String.format("|   HEAPSORT    | Quase ordenado |     %.3f    |     %.3f    |", tempoAleatorio, tempoOrdenado);
+        System.out.println(mensagem);
+        
+
+        // RADIXSORT
+        RadixSort rs = new RadixSort();
+        int size = vetorAleatorio.length;
+
+        start = System.currentTimeMillis();
+        rs.radixSort(vetorAleatorio, size);
+        tempoAleatorio = (float) (System.currentTimeMillis() - start) / 1000;
+
+        start = System.currentTimeMillis();
+        rs.radixSort(vetorDescente, size);
+        tempoOrdenado = (float) (System.currentTimeMillis() - start) / 1000;
+
+        mensagem = String.format("|   RADIXSORT   | Quase ordenado |     %.3f    |     %.3f    |", tempoAleatorio, tempoOrdenado);
+        System.out.println(mensagem);
 
     }
         
--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,36 +1,54 @@
 public class Quicksort {
 
-    public static final int length = 0;
+    // method to find the partition position
+  static int partition(int array[], int low, int high) {
+    
+    // choose the rightmost element as pivot
+    int pivot = array[high];
+    
+    // pointer for greater element
+    int i = (low - 1);
 
-    public static void quickSort(int[] arr, int start, int end){
- 
-        int partition = partition(arr, start, end);
- 
-        if(partition-1>start) {
-            quickSort(arr, start, partition - 1);
-        }
-        if(partition+1<end) {
-            quickSort(arr, partition + 1, end);
-        }
+    // traverse through all elements
+    // compare each element with pivot
+    for (int j = low; j < high; j++) {
+      if (array[j] <= pivot) {
+
+        // if element smaller than pivot is found
+        // swap it with the greatr element pointed by i
+        i++;
+
+        // swapping element at i with element at j
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+
     }
- 
-    public static int partition(int[] arr, int start, int end){
-        int pivot = arr[end];
- 
-        for(int i=start; i<end; i++){
-            if(arr[i]<pivot){
-                int temp= arr[start];
-                arr[start]=arr[i];
-                arr[i]=temp;
-                start++;
-            }
-        }
- 
-        int temp = arr[start];
-        arr[start] = pivot;
-        arr[end] = temp;
- 
-        return start;
+
+    // swapt the pivot element with the greater element specified by i
+    int temp = array[i + 1];
+    array[i + 1] = array[high];
+    array[high] = temp;
+
+    // return the position from where partition is done
+    return (i + 1);
+  }
+
+  static void quickSort(int array[], int low, int high) {
+    if (low < high) {
+
+      // find pivot element such that
+      // elements smaller than pivot are on the left
+      // elements greater than pivot are on the right
+      int pi = partition(array, low, high);
+      
+      // recursive call on the left of pivot
+      quickSort(array, low, pi - 1);
+
+      // recursive call on the right of pivot
+      quickSort(array, pi + 1, high);
     }
+  }
 
 }
